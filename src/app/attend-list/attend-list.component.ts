64227d1b4c6e7d34d1c502ccc18d569541a5d942
empelt <tetsuyakato2001@yahoo.co.jp>
2020-04-27 17:32:27 +0900

    first commit

--- src/app/attend-list/attend-list.component.ts
+++ src/app/attend-list/attend-list.component.ts
@@ -10,24 +10,171 @@ export class AttendListComponent implements OnInit {
 
   attendlist = [
     {classnum: 11, data: [
-      {no: 1, name: "山田花子", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 2, name: "山田花子", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 3, name: "山田花子", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 4, name: "山田花子", absent: 0, late: 3, early: 0, attend: 15},
+      {no: 1, name: "山田花子", selectIndex: 0},
+      {no: 2, name: "山田花子", selectIndex: 0},
+      {no: 3, name: "山田花子", selectIndex: 0},
+      {no: 4, name: "山田花子", selectIndex: 0},
     ]
     },
     {classnum: 12, data: [
-      {no: 1, name: "山田太郎", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 2, name: "山田太郎", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 3, name: "山田太郎", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 4, name: "山田太郎", absent: 0, late: 3, early: 0, attend: 15},
+      {no: 1, name: "山田太郎", selectIndex: 0},
+      {no: 2, name: "山田太郎", selectIndex: 0},
+      {no: 3, name: "山田太郎", selectIndex: 0},
+      {no: 4, name: "山田太郎", selectIndex: 0},
     ]
     },
     {classnum: 13, data: [
-      {no: 1, name: "山田", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 2, name: "山田", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 3, name: "山田", absent: 0, late: 3, early: 0, attend: 15},
-      {no: 4, name: "山田", absent: 0, late: 3, early: 0, attend: 15},
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 14, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 15, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 16, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 17, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 18, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 21, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 22, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 23, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 24, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 25, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 26, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 27, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 28, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 31, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 32, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 33, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 34, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 35, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 36, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 37, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
+    ]
+    },
+    {classnum: 38, data: [
+      {no: 1, name: "山田", selectIndex: 0},
+      {no: 2, name: "山田", selectIndex: 0},
+      {no: 3, name: "山田", selectIndex: 0},
+      {no: 4, name: "山田", selectIndex: 0},
     ]
     },
   ]
@@ -40,6 +187,10 @@ export class AttendListComponent implements OnInit {
   ngOnInit(): void {
     this.classnum = Number(this.route.snapshot.queryParamMap.get('classnum'));
     this.viewlist = this.attendlist.find((v) => v.classnum == this.classnum).data;
+  }
+
+  onClick(index: number, no: number) {
+    this.attendlist.find((v) => v.classnum == this.classnum).data.find((v) => v.no == no).selectIndex = index;
   }
 
 
